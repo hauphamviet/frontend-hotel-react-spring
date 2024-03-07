b71e77d0de4481b6a3bf30cdb757ab90f1c5425d
Hau <pvhau2002@gmail.com>
2024-03-07 20:53:09 +0700

    frontend react

---
+++ lakeSide-hotel/src/App.jsx
@@ -0,0 +1,53 @@
+import { useState } from 'react'
+import AddRoom from './components/room/AddRoom'
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css" //
+import "/node_modules/bootstrap/dist/js/bootstrap.min.js" // import o day thi no moi nhan duoc css
+import ExistingRooms from './components/room/ExistingRooms'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+import EditRoom from './components/room/EditRoom'
+import Home from './components/home/Home'
+import NavBar from './components/layout/NavBar'
+import Footer from './components/layout/Footer'
+import RoomListing from './components/room/RoomListing'
+import Admin from './components/admin/Admin'
+import Checkout from './components/booking/Checkout'
+import BookingSuccess from './components/booking/BookingSuccess'
+import Bookings from './components/booking/Bookings'
+import FindBooking from './components/booking/FindBooking'
+import { AuthProvider } from './components/auth/AuthProvider'
+import Login from './components/auth/Login'
+import Registration from './components/auth/Registration'
+import Profile from './components/auth/Profile'
+
+function App() {
+  return (
+    <AuthProvider>
+    <main>
+      <Router>
+        <NavBar />
+        <Routes>
+          <Route path='/' element={<Home/>}/>
+          <Route path='/edit-room/:roomId' element={<EditRoom/>}/>
+          <Route path='/existing-rooms' element={<ExistingRooms/>}/>
+          <Route path='/add-room' element={<AddRoom/>}/>
+          <Route path='/book-room/:roomId' element={<Checkout/>}/>
+          <Route path='/browse-all-rooms' element={<RoomListing/>}/>
+          <Route path='/admin' element={<Admin />} />
+          <Route path='/booking-success' element={<BookingSuccess />} />
+          <Route path='/existing-bookings' element={<Bookings />} />
+          <Route path='/find-booking' element={<FindBooking />} />
+
+          <Route path='/login' element={<Login />} />
+          <Route path='/register' element={<Registration />} />
+
+          <Route path='/profile' element={<Profile />} />
+          <Route path='/logout' element={<FindBooking />} />
+        </Routes>
+      </Router>
+      <Footer />
+    </main>
+    </AuthProvider>
+  )
+}
+
+export default App
